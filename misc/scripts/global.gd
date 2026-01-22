f26f8e9dfafb26c6c0f60773a5975fea27b3dec1
dalifyyy <d4lified@gmail.com>
2026-01-22 19:48:59 +0800

    ..

--- misc/scripts/global.gd
+++ misc/scripts/global.gd
@@ -1,7 +1,13 @@
 extends Node
 
-func get_main(): return get_tree().current_scene
-func get_group_node(group): return get_tree().get_first_node_in_group(group)
+func get_main(): 
+	return get_tree().current_scene
+	
+func get_group_node(group): 
+	return get_tree().get_first_node_in_group(group)
 
 func change_scene(path:String):
 	get_tree().change_scene_to_file(path)
+
+func input(id):
+	return Input.is_action_pressed(id)
---
+++ objects/player/player.gd
@@ -0,0 +1,53 @@
+extends CharacterBody3D
+
+@export var speed: float = 220
+@export var look_sensitivity: float = 0.2
+
+var can_move = true
+var mouse_delta := Vector2.ZERO
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) 
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		mouse_delta = event.relative * look_sensitivity
+
+func _physics_process(delta):
+	$Camera3D.rotation_degrees.y -= mouse_delta.x
+	$Camera3D.rotation_degrees.x -= mouse_delta.y 
+	$Camera3D.rotation_degrees.x = clamp($Camera3D.rotation_degrees.x, -50, 40)
+	$model.rotation_degrees.y = $Camera3D.rotation_degrees.y
+	mouse_delta = Vector2.ZERO 
+	
+	var direction = Vector3.ZERO
+	if can_move:
+		if Global.input("forward"):
+			direction -= $Camera3D.transform.basis.z 
+		if Global.input("backward"):
+			direction += $Camera3D.transform.basis.z
+		if Global.input("left"):
+			direction -= $Camera3D.transform.basis.x
+		if Global.input("right"):
+			direction += $Camera3D.transform.basis.x
+		
+		#if Global.input("forward") or Global.input("backward") or Global.input("left") or Global.input("right"):
+			#$model/AnimationPlayer.play("walk")
+		#else:
+			#$model/AnimationPlayer.play("RESET")
+	
+	if direction != Vector3.ZERO:
+		direction = direction.normalized() * speed * delta
+	
+	velocity.x = direction.x
+	velocity.z = direction.z
+
+	move_and_slide()
+
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_cancel"):  
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	
